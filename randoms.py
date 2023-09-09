62ce07329f1f4bd50235189eee209ddc0dc9dc36
Dubem <dubemmegbo@gmail.com>
2023-09-10 00:59:32 +0100

    Playing with and learning regEx

--- randoms.py
+++ randoms.py
@@ -4,3 +4,9 @@ fruit = ['blueberry','apple','cherry','banana']
 print(colors[0])
 
 print (colors[0],fruit[0], colors[1], fruit[1], colors[2], fruit[2], colors[3], fruit[3])
+
+import re
+
+
+print(re.findall(r'[Gg]eek', 'GeeksforGeeks: \
+				A computer science portal for geeks'))
